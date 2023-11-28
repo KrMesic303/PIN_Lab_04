ab8beeeb6e555c1dd22f83f23c47b17271215893
Kristian Mesić <kristian.mesic303@gmail.com>
2023-11-28 15:47:13 +0100

    Add project files.

---
+++ MoviesApp/Data/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MoviesApp.Data
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Genre { get; set; } = string.Empty;
+
+
+    }
+}
